02c94540bfcb9b1f962681941b7edacb0db761a3
Patrick Reisert <kpreisert@gmail.com>
2017-07-14 17:21:10 +0200

    More CoreMIDI support
    This is still incomplete and has bugs.

--- src/backend/coremidi/mod.rs
+++ src/backend/coremidi/mod.rs
@@ -1,6 +1,8 @@
 #![allow(dead_code)]
 #![allow(unused_variables)]
 
+use std::sync::{Arc, Mutex};
+
 use ::errors::*;
 use ::Ignore;
 
@@ -40,11 +42,25 @@ impl MidiInput {
         where F: FnMut(f64, &[u8], &mut T) + Send + 'static {
         // TODO: handle failure of from_index for invalid index
         let src = Source::from_index(port_number);
-        let data_ref = &mut data;
+
+        let handler_data = Arc::new(Mutex::new(HandlerData {
+            ignore_flags: self.ignore_flags,
+            callback: Box::new(callback),
+            user_data: Some(data)
+        }));
+        let handler_data2 = handler_data.clone();
         let port = match self.client.input_port(port_name, move |packets| {
-            // TODO: filtering; maybe merge SysEx (if they can be split)
+            // TODO: implement message filtering
+            // TODO: calculate timestamps
+            // TODO: maybe merge SysEx (if they can be split)
+            // TODO: synchronization is required because the borrow checker does not
+            //       know that the callback we're in here is never called concurrently
+            //       (always in sequence)
+            let mut data_locked = handler_data2.lock().unwrap();
+            let dataref: &mut HandlerData<T> = &mut *data_locked;
+            let data = &mut dataref.user_data.as_mut().unwrap();
             for p in packets.iter() {
-                callback(0.0, p.data(), data_ref)
+                (dataref.callback)(0.0, p.data(), data)
             }
         }) {
             Ok(p) => p,
@@ -56,18 +72,45 @@ impl MidiInput {
         Ok(MidiInputConnection {
             client: self.client,
             details: InputConnectionDetails::Explicit(port),
-            handler_data: HandlerData {
-                ignore_flags: self.ignore_flags,
-                user_data: data
-            }
+            handler_data: handler_data
         })
     }
 
-    pub fn create_virtual<F, T: Send>(
+    // FIXME: get rid of code duplication with above method
+    // FIXME: the test_virtual example currently fails for some reason 
+    pub fn create_virtual<F, T: Send + 'static>(
         self, port_name: &str, callback: F, data: T
     ) -> Result<MidiInputConnection<T>, ConnectError<MidiInput>>
     where F: FnMut(f64, &[u8], &mut T) + Send + 'static {
-        unimplemented!()
+
+        let handler_data = Arc::new(Mutex::new(HandlerData {
+            ignore_flags: self.ignore_flags,
+            callback: Box::new(callback),
+            user_data: Some(data)
+        }));
+        let handler_data2 = handler_data.clone();
+        let vrt = match self.client.virtual_destination(port_name, move |packets| {
+            // TODO: implement message filtering
+            // TODO: calculate timestamps
+            // TODO: maybe merge SysEx (if they can be split)
+            // TODO: synchronization is required because the borrow checker does not
+            //       know that the callback we're in here is never called concurrently
+            //       (always in sequence)
+            let mut data_locked = handler_data2.lock().unwrap();
+            let dataref: &mut HandlerData<T> = &mut *data_locked;
+            let data = &mut dataref.user_data.as_mut().unwrap();
+            for p in packets.iter() {
+                (dataref.callback)(0.0, p.data(), data)
+            }
+        }) {
+            Ok(p) => p,
+            Err(_) => return Err(ConnectError::other("error creating MIDI input port", self))
+        };
+        Ok(MidiInputConnection {
+            client: self.client,
+            details: InputConnectionDetails::Virtual(vrt),
+            handler_data: handler_data
+        })
     }
 }
 
@@ -79,7 +122,17 @@ enum InputConnectionDetails {
 pub struct MidiInputConnection<T> {
     client: Client,
     details: InputConnectionDetails,
-    handler_data: HandlerData<T>
+    handler_data: Arc<Mutex<HandlerData<T>>> // TODO: get rid of Arc & Mutex?
+}
+
+impl<T> MidiInputConnection<T> {
+    pub fn close(self) -> (MidiInput, T) {
+        let mut handler_data_locked = self.handler_data.lock().unwrap();
+        (MidiInput {
+            client: self.client,
+            ignore_flags: handler_data_locked.ignore_flags
+        }, handler_data_locked.user_data.take().unwrap())
+    }
 }
 
 /// This is all the data that is stored on the heap as long as a connection
@@ -90,14 +143,8 @@ pub struct MidiInputConnection<T> {
 struct HandlerData<T> {
     //last_time: Option<u64>,
     ignore_flags: Ignore,
-    //callback: Box<FnMut(f64, &[u8], &mut T)+Send>,
-    user_data: T //Option<T>
-}
-
-impl<T> MidiInputConnection<T> {
-    pub fn close(self) -> (MidiInput, T) {
-        (MidiInput { client: self.client, ignore_flags: self.handler_data.ignore_flags }, self.handler_data.user_data)
-    }
+    callback: Box<FnMut(f64, &[u8], &mut T)+Send>,
+    user_data: Option<T>
 }
 
 pub struct MidiOutput {
