1f2f4989e296793bf5e8a04f4d7c40bd3d03295e
Patrick Reisert <kpreisert@gmail.com>
2017-12-28 13:35:23 +0100

    Add looping mode to test_list_ports

--- examples/test_list_ports.rs
+++ examples/test_list_ports.rs
@@ -1,5 +1,6 @@
 extern crate midir;
 
+use std::io::{stdin, stdout, Write};
 use std::error::Error;
 
 use midir::{MidiInput, MidiOutput, Ignore};
@@ -15,15 +16,30 @@ fn run() -> Result<(), Box<Error>> {
     let mut midi_in = MidiInput::new("midir test input")?;
     midi_in.ignore(Ignore::None);
     let midi_out = MidiOutput::new("midir test output")?;
-    
-    println!("Available input ports:");
-    for i in 0..midi_in.port_count() {
-        println!("{}: {}", i, midi_in.port_name(i)?);
-    }
-    
-    println!("\nAvailable output ports:");
-    for i in 0..midi_out.port_count() {
-        println!("{}: {}", i, midi_out.port_name(i)?);
+
+    let mut input = String::new();
+
+    loop {
+        println!("Available input ports:");
+        for i in 0..midi_in.port_count() {
+            println!("{}: {}", i, midi_in.port_name(i)?);
+        }
+        
+        println!("\nAvailable output ports:");
+        for i in 0..midi_out.port_count() {
+            println!("{}: {}", i, midi_out.port_name(i)?);
+        }
+
+        // run in endless loop if "--loop" parameter is specified
+        match ::std::env::args().nth(1) {
+            Some(ref arg) if arg == "--loop" => {}
+            _ => break
+        }
+        print!("\nPress <enter> to retry ...");
+        stdout().flush()?;
+        input.clear();
+        stdin().read_line(&mut input)?;
+        println!("\n");
     }
     
     Ok(())
