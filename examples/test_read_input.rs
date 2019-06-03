0236ba159394d4bce3b35a8b4517643006104d65
Josh Mcguigan <joshmcg88@gmail.com>
2019-06-03 05:19:41 -0700

    add read input example

---
+++ examples/test_read_input.rs
@@ -0,0 +1,56 @@
+extern crate midir;
+
+use std::io::{stdin, stdout, Write};
+use std::error::Error;
+
+use midir::{MidiInput, Ignore};
+
+fn main() {
+    match run() {
+        Ok(_) => (),
+        Err(err) => println!("Error: {}", err.description())
+    }
+}
+
+fn run() -> Result<(), Box<Error>> {
+    let mut input = String::new();
+    
+    let mut midi_in = MidiInput::new("midir reading input")?;
+    midi_in.ignore(Ignore::None);
+    
+    // Get an input port (read from console if multiple are available)
+    let in_port = match midi_in.port_count() {
+        0 => return Err("no input port found".into()),
+        1 => {
+            println!("Choosing the only available input port: {}", midi_in.port_name(0).unwrap());
+            0
+        },
+        _ => {
+            println!("\nAvailable input ports:");
+            for i in 0..midi_in.port_count() {
+                println!("{}: {}", i, midi_in.port_name(i).unwrap());
+            }
+            print!("Please select input port: ");
+            stdout().flush()?;
+            let mut input = String::new();
+            stdin().read_line(&mut input)?;
+            input.trim().parse()?
+        }
+    };
+    
+    println!("\nOpening connections");
+    let in_port_name = midi_in.port_name(in_port)?;
+
+    // _conn_in needs to be a named parameter, because it needs to be kept alive until the end of the scope
+    let _conn_in = midi_in.connect(in_port, "midir-read-input", move |stamp, message, _| {
+        println!("{}: {:?} (len = {})", stamp, message, message.len());
+    }, ())?;
+    
+    println!("Connections open, reading input from '{}' (press enter to exit) ...", in_port_name);
+
+    input.clear();
+    stdin().read_line(&mut input)?; // wait for next enter key press
+
+    println!("Closing connections");
+    Ok(())
+}
