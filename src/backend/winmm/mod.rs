e55df3db1181545b2f1e59ca2e1f3a79aff9ce5b
Henrik Rydgård <hrydgard@gmail.com>
2022-06-02 14:33:35 +0200

    Change allocation minimum alignment to 1 byte, 16 bytes is not necessary for sysex

--- src/backend/winmm/mod.rs
+++ src/backend/winmm/mod.rs
@@ -223,7 +223,7 @@ impl MidiInput {
         // Allocate and init the sysex buffers.
         for i in 0..MIDIR_SYSEX_BUFFER_COUNT {
             handler_data.sysex_buffer.0[i] = Box::into_raw(Box::new(MIDIHDR {
-                lpData: PSTR(unsafe { alloc(Layout::from_size_align_unchecked(MIDIR_SYSEX_BUFFER_SIZE, 16)) }),
+                lpData: PSTR(unsafe { alloc(Layout::from_size_align_unchecked(MIDIR_SYSEX_BUFFER_SIZE, 1)) }),
                 dwBufferLength: MIDIR_SYSEX_BUFFER_SIZE as u32,
                 dwBytesRecorded: 0,
                 dwUser: i as DWORD_PTR, // We use the dwUser parameter as buffer indicator
@@ -290,7 +290,7 @@ impl<T> MidiInputConnection<T> {
             let result;
             unsafe {
                 result = midiInUnprepareHeader(*in_handle_lock, self.handler_data.sysex_buffer.0[i], mem::size_of::<MIDIHDR>() as u32);
-                dealloc((*self.handler_data.sysex_buffer.0[i]).lpData.0 as *mut _, Layout::from_size_align_unchecked(MIDIR_SYSEX_BUFFER_SIZE, 16));
+                dealloc((*self.handler_data.sysex_buffer.0[i]).lpData.0 as *mut _, Layout::from_size_align_unchecked(MIDIR_SYSEX_BUFFER_SIZE, 1));
                 // recreate the Box so that it will be dropped/deallocated at the end of this scope
                 let _ = Box::from_raw(self.handler_data.sysex_buffer.0[i]);
             }
